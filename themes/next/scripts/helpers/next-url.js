e72f18ad0ed302a65868cb8f4caac81e91e01b8e
smaIIstars <smallstars.he@qq.com>
2021-08-16 00:41:24 +0800

    feat(blog): theme switching

--- themes/next/scripts/helpers/next-url.js
+++ themes/next/scripts/helpers/next-url.js
@@ -1,46 +1,45 @@
 /* global hexo */
 
-'use strict';
+"use strict";
 
-const { htmlTag } = require('hexo-util');
-const url = require('url');
+const { htmlTag } = require("hexo-util");
+const url = require("url");
 
-hexo.extend.helper.register('next_url', function(path, text, options = {}) {
+hexo.extend.helper.register("next_url", function (path, text, options = {}) {
   const { config } = this;
   const data = url.parse(path);
   const siteHost = url.parse(config.url).hostname || config.url;
 
   const theme = hexo.theme.config;
-  let exturl = '';
-  let tag = 'a';
+  let exturl = "";
+  let tag = "a";
   let attrs = { href: this.url_for(path) };
 
   // If `exturl` enabled, set spanned links only on external links.
   if (theme.exturl && data.protocol && data.hostname !== siteHost) {
-    tag = 'span';
-    exturl = 'exturl';
-    const encoded = Buffer.from(path).toString('base64');
+    tag = "span";
+    exturl = "exturl";
+    const encoded = Buffer.from(path).toString("base64");
     attrs = {
-      class     : exturl,
-      'data-url': encoded
+      class: exturl,
+      "data-url": encoded,
     };
   }
 
   for (let key in options) {
-
     /**
      * If option have `class` attribute, add it to
      * 'exturl' class if `exturl` option enabled.
      */
-    if (exturl !== '' && key === 'class') {
-      attrs[key] += ' ' + options[key];
+    if (exturl !== "" && key === "class") {
+      attrs[key] += " " + options[key];
     } else {
       attrs[key] = options[key];
     }
   }
 
   if (attrs.class && Array.isArray(attrs.class)) {
-    attrs.class = attrs.class.join(' ');
+    attrs.class = attrs.class.join(" ");
   }
 
   // If it's external link, rewrite attributes.
@@ -49,8 +48,8 @@ hexo.extend.helper.register('next_url', function(path, text, options = {}) {
 
     if (!theme.exturl) {
       // Only for simple link need to rewrite/add attributes.
-      attrs.rel = 'noopener';
-      attrs.target = '_blank';
+      attrs.rel = "noopener";
+      attrs.target = "_blank";
     } else {
       // Remove rel attributes for `exturl` in main menu.
       attrs.rel = null;
